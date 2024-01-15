b969aa6537bd42f09378d5349ab4b6152107149c
Hermilo Acuna <hacuna100@gmail.com>
2024-01-15 08:47:45 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, 
+    RouterOutlet, 
+    NgbModule, 
+    NavbarComponent, 
+    HomeComponent,
+    AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [NgbModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
